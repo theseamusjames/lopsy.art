34d0825e566aafe9efb7dede2c9cd27919afeb79
theseamusjames <theseamusjames@gmail.com>
2026-05-03 23:40:56 -0700

    fix(marquee): use existing OptionsBar styles instead of missing CSS module (#317)

--- src/app/OptionsBar/tool-options/MarqueeOptions.tsx
+++ src/app/OptionsBar/tool-options/MarqueeOptions.tsx
@@ -1,26 +1,15 @@
 import { AspectRatioControl } from './AspectRatioControl';
 import { Slider } from '../../../components/Slider/Slider';
 import { useToolSettingsStore } from '../../tool-settings-store';
-import styles from './ToolOptions.module.css';
 
 export function MarqueeOptions() {
   const marqueeFeather = useToolSettingsStore((s) => s.marqueeFeather);
   const setMarqueeFeather = useToolSettingsStore((s) => s.setMarqueeFeather);
 
   return (
-    <div className={styles.container}>
+    <>
       <AspectRatioControl />
-      <div className={styles.sliderGroup}>
-        <label className={styles.label}>Feather</label>
-        <Slider
-          value={marqueeFeather}
-          onChange={setMarqueeFeather}
-          min={0}
-          max={250}
-          step={1}
-        />
-        <span className={styles.value}>{marqueeFeather} px</span>
-      </div>
-    </div>
+      <Slider label="Feather" value={marqueeFeather} min={0} max={250} onChange={setMarqueeFeather} />
+    </>
   );
 }
